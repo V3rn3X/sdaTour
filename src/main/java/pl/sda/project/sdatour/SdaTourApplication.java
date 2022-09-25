0c675ef68483bbed1f48f976608e04730faeceb0
Hunter <hunterpassaro@gmail.com>
2022-09-25 13:47:24 +0100

    Initial commit

---
+++ src/main/java/pl/sda/project/sdatour/SdaTourApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.project.sdatour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/project/sdatour/SdaTourApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.project.sdatour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
